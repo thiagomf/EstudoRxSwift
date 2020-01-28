69e3946494f654e1a4804bc3a3e2d47d98c37d5f
Thiago Machado <thiagomachadofaria@hotmail.com>
2020-01-28 17:33:32 -0300

    Novos exemplos

---
+++ EstudoRxSwift/EstudoRXSwift.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,72 @@
+import UIKit
+import RxSwift
+
+example(of: "toArray") {
+    
+    let disposeBag = DisposeBag()
+    
+    Observable.of("A", "B", "C")
+        .toArray()
+        .subscribe {
+            print($0)
+        }
+        .disposed(by: disposeBag)
+}
+
+example(of: "map") {
+    
+    let dispose = DisposeBag()
+    
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .spellOut
+    
+    Observable<NSNumber>.of(123,4,56)
+        .map {
+            formatter.string(from: $0) ?? ""
+        }
+    .subscribe(onNext: {
+        print($0)
+    })
+        .disposed(by: dispose)
+}
+
+example(of: "mapWithIndex") {
+    
+    let dispose = DisposeBag()
+    
+    Observable.of(1,2,3,4,5,6,7,8,9)
+    .mapWithIndex { integer , index in
+        index > 1 ? integer * 2 : integer
+    }
+    .subscribe (onNext: {
+        print($0)
+    })
+    .disposed(by: dispose)
+}
+
+struct Student {
+    
+    var score: Variable<Int>
+}
+
+example(of: "flapMap") {
+    
+    let dispose = DisposeBag()
+    
+    let ryan = Student(score: Variable(80))
+    
+    let charlotte = Student(score: Variable(90))
+    
+    let student = PublishSubject<Student>()
+    
+    student.asObservable()
+    .flapMap {
+        $0.score.asObservable()
+    }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: dispose)
+    
+    student.onNext(ryan)
+}
